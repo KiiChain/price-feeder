a27251966e6c32ed34295b7744d72ef6c15ec7cb
getcakedieyoung <164500857+getcakedieyoungx@users.noreply.github.com>
2025-07-16 19:59:37 +0300

    Create testconfig.go

---
+++ config/testconfig.go
@@ -0,0 +1,19 @@
+// ... existing code ...
+
+// ProviderEndpoint defines an override setting in our config for the
+// hardcoded rest and websocket api endpoints.
+type ProviderEndpoint struct {
+    // Name of the provider, ex. "binance"
+    Name string `toml:"name"`
+
+    // Rest endpoint for the provider, ex. "https://api1.binance.com"
+    Rest string `toml:"rest"`
+
+    // Websocket endpoint for the provider, ex. "stream.binance.com:9443"
+    Websocket string `toml:"websocket"`
+
+    // Timeout for this provider, ex. "200ms"
+    Timeout string `toml:"timeout"`
+}
+
+// ... existing code ...
